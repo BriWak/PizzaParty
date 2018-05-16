3e2411da890e1494ba59d8eaddf194194768b1c0
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-05-16 13:27:15 +0100

    refactored tu use more functions inside case class

--- src/main/scala/PizzaParty.scala
+++ src/main/scala/PizzaParty.scala
@@ -2,16 +2,30 @@ object PizzaParty{
 
   def apply(apprentices: Int, order: PizzaOrder) : String = {
 
-    val orderAsDecimals = order.slices.map(x => x.head.asDigit.toDouble / x.last.asDigit.toDouble)
-    val moreThanOne = if (apprentices > 1) "s" else ""
-    val pizzas = orderAsDecimals.sum.ceil.toInt
-    val multiPizzas = if (pizzas > 1) "s" else ""
-    val remainder = ((pizzas - orderAsDecimals.sum) * 8).toInt
+    val pizzas = order.wholePizzas
+    val remainder = order.leftovers
+
+    val addS = if (apprentices > 1) "s" else ""
+    val pluralPizzas = if (pizzas > 1) "s" else ""
     val leftovers = if (remainder == 0) "no" else remainder
     val nonPizzaEaters = apprentices - order.slices.length
     val pizzaHaters = if (nonPizzaEaters <= 0) "" else s" There are $nonPizzaEaters apprentices that don't want pizza!"
-    s"To feed $apprentices hungry apprentice$moreThanOne, I need $pizzas pizza$multiPizzas. I have $leftovers leftover slices!$pizzaHaters"
+
+    s"To feed $apprentices hungry apprentice$addS, I need $pizzas pizza$pluralPizzas. I have $leftovers leftover slices!$pizzaHaters"
   }
 }
 
-case class PizzaOrder(slices: String*)
+case class PizzaOrder(slices: String*) {
+
+  def asDoubles: Seq[Double] = {
+    slices.map(x =>  x.head.asDigit.toDouble / x.last.asDigit.toDouble)
+  }
+
+  def wholePizzas: Int = {
+    this.asDoubles.sum.ceil.toInt
+  }
+
+  def leftovers: Int = {
+    ((this.wholePizzas - this.asDoubles.sum) * 8).toInt
+  }
+}
